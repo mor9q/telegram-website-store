890f62406033459e562a9aeee132e7134905694f
mor9q <radmir06x@mail.ru>
2025-01-11 23:00:10 +0300

    receiving and processing data from web page

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import logging
+import json
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
@@ -15,12 +16,19 @@ dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def start(message: types.Message):
-    markup = types.InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Открыть веб-страницу", url='https://mor9q.github.io/telegram-website-store/')]
+    markup = types.ReplyKeyboardMarkup(inline_keyboard=[
+        [KeyboardButton(text="Открыть веб-страницу", web_app=WebAppInfo(url='https://mor9q.github.io/telegram-website-store/'))]
     ])
 
     await message.answer('Привет мой друг!', reply_markup=markup)
 
+
+@dp.message(lambda msg: msg.web_app_data is not None)
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Name: {res['name']}\n Email: {res['email']}\n Phone: {res['phone']}')
+    await message.answer('Вы зарегистрировались!')
+    
 
 async def main():
     await dp.start_polling(bot)
